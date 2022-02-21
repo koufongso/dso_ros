aa7bc1c1d3ad91405fb76263291f2b5a52d8e2e1
Gaofeng <koufongso@gmail.com>
2022-02-20 23:18:24 -0800

    added ros PointCloud2 msg publisher

--- src/RosPublisher.h
+++ src/RosPublisher.h
@@ -56,7 +56,7 @@ class FrameShell;
                     // need to check 
                     auto q = frame->camToWorld.unit_quaternion();
                     camPose_w.transform.rotation.x = q.x();
-                    camPose_w.transform.rotation.y = -q.z();
+                    camPose_w.transform.rotation.y = q.z();
                     camPose_w.transform.rotation.z = -q.y();
                     camPose_w.transform.rotation.w = q.w();
 
@@ -67,87 +67,82 @@ class FrameShell;
 
 
                 // still need to fix the pointCloud publisher
-                // void publishKeyframes( std::vector<FrameHessian*> &frames, bool final, CalibHessian* HCalib) override {
-                //     if(final){
-                //         //id = fh->shell->id;
-                //         float fx = HCalib->fxl();
-                //         float fy = HCalib->fyl();
-                //         float cx = HCalib->cxl();
-                //         float cy = HCalib->cyl();
-                //         //width = wG[0];
-                //         //height = hG[0];
-                //         float fxi = 1/fx;
-                //         float fyi = 1/fy;
-                //         float cxi = -cx / fx;
-                //         float cyi = -cy / fy;
-                //         int count = 0;
-                //         for (FrameHessian* fh : frames){
-                //             auto camToWorld = fh->shell->camToWorld.matrix().cast<float>();
+                void publishKeyframes( std::vector<FrameHessian*> &frames, bool final, CalibHessian* HCalib) override {
+                   
+                    //id = fh->shell->id;
+                    float fx = HCalib->fxl();
+                    float fy = HCalib->fyl();
+                    float cx = HCalib->cxl();
+                    float cy = HCalib->cyl();
+                    //width = wG[0];
+                    //height = hG[0];
+                    float fxi = 1/fx;
+                    float fyi = 1/fy;
+                    float cxi = -cx / fx;
+                    float cyi = -cy / fy;
+                    int count = 0;
+                    for (FrameHessian* fh : frames){
+                        auto camToWorld = fh->shell->camToWorld.matrix().cast<float>();
 
-                //             // sensor_msgs::PointCloud2 pc_msg;
-                //             // pc_msg.header.frame_id = "world";
-                //             // pc_msg.is_bigendian = false;
-                //             // pc_msg.height=1;
-                //             // pc_msg.width = fh->pointHessians.size();
-                //             // pc_msg.point_step = 12;
-                //             // pc_msg.is_dense = true;
-                //             //pc_msg.row_step = pc_msg.width * pc_msg.point_step;
+                        sensor_msgs::PointCloud2 pc_msg;
+                        pc_msg.header.frame_id = "world";
+                        pc_msg.is_bigendian = false;
+                        pc_msg.height=1;
+                        pc_msg.width = fh->pointHessians.size();
+                        pc_msg.point_step = 12;
+                        pc_msg.is_dense = true;
+                        pc_msg.row_step = pc_msg.width * pc_msg.point_step;
 
-                //             // pc_msg.fields.resize(3);
-                //             // pc_msg.fields[0].name = "x";
-                //             // pc_msg.fields[0].offset = 0;
-                //             // pc_msg.fields[0].datatype = sensor_msgs::PointField::FLOAT32;
-                //             // pc_msg.fields[0].count = 1;
+                        pc_msg.fields.resize(3);
+                        pc_msg.fields[0].name = "x";
+                        pc_msg.fields[0].offset = 0;
+                        pc_msg.fields[0].datatype = sensor_msgs::PointField::FLOAT32;
+                        pc_msg.fields[0].count = 1;
 
-                //             // pc_msg.fields[11].name = "y";
-                //             // pc_msg.fields[1].offset = 4;
-                //             // pc_msg.fields[1].datatype = sensor_msgs::PointField::FLOAT32;
-                //             // pc_msg.fields[1].count = 1;
+                        pc_msg.fields[1].name = "y";
+                        pc_msg.fields[1].offset = 4;
+                        pc_msg.fields[1].datatype = sensor_msgs::PointField::FLOAT32;
+                        pc_msg.fields[1].count = 1;
 
-                //             // pc_msg.fields[2].name = "z";
-                //             // pc_msg.fields[2].offset = 8;
-                //             // pc_msg.fields[2].datatype = sensor_msgs::PointField::FLOAT32;
-                //             // pc_msg.fields[2].count = 1;
+                        pc_msg.fields[2].name = "z";
+                        pc_msg.fields[2].offset = 8;
+                        pc_msg.fields[2].datatype = sensor_msgs::PointField::FLOAT32;
+                        pc_msg.fields[2].count = 1;
 
 
-                //             // pc_msg.data.resize(12*pc_msg.width);
+                        pc_msg.data.resize(12*pc_msg.width);
+                        
+                        int p0 = 0; //starting position of each point
+                        for (PointHessian* ph : fh->pointHessians){
+
+                            float depth = 1.0f/(ph->idepth_scaled);                           
+                            float x = (ph->u * fxi + cxi) * depth;
+                            float y = (ph->v * fyi + cyi) * depth;
+                            float z = (1+2*fxi*(rand()/(float)RAND_MAX-0.5f)) * depth ;
+                            Eigen::Vector4f p_cam(x,y,z,1.0); // column vector
+                            Eigen::Vector4f p_world = camToWorld*p_cam;
                             
-                //             int offset = 0;
-                //             for (PointHessian* ph : fh->pointHessians){
+                            x = p_world(0);
+                            y = p_world(2);
+                            z = -p_world(1);
 
-                //                 float depth = 1.0f/(ph->idepth_scaled);                           
-                //                 float x = (ph->u * fxi + cxi) * depth;
-                //                 float y = (ph->v * fyi + cyi) * depth;
-                //                 float z = ((1+2*fxi)*(rand()/(float)RAND_MAX-0.5f)) * depth ;
-                //                 Eigen::Vector4f p_cam(x,y,z,1.0); // column vector
-                //                 Eigen::Vector4f p_world = camToWorld*p_cam;
-                                
-                //                 x = p_world(1);
-                //                 y = p_world(2);
-                //                 z = p_world(3);
+                            uint8_t *castPtr_x = reinterpret_cast<uint8_t*>(&x);
+                            uint8_t *castPtr_y = reinterpret_cast<uint8_t*>(&y);
+                            uint8_t *castPtr_z = reinterpret_cast<uint8_t*>(&z);
 
-                //                 // float x = static_cast<float>(i)+0.7;
-                //                 // float y = x;
-                //                 // float z = x*x+y*y;
-                //                 printf("%f,%f,%f\n",x,y,z);
-                //                 fprintf (outputFile,"%f,%f,%f\n",x,y,z);
-
-                //                 //float *castPtr_x = reinterpret_cast<float*>(&x);
-                //                 //float *castPtr_y = reinterpret_cast<float*>(&y);
-                //                 //float *castPtr_z = reinterpret_cast<float*>(&z);
-
-                //                 // for (int pos=0; pos<4;pos++){
-                //                 //     pc_msg.data[offset+pos] = castPtr_x[pos];
-                //                 //     pc_msg.data[offset+4+pos] = castPtr_y[pos];
-                //                 //     pc_msg.data[offset+8+pos] = castPtr_z[pos];
-                //                 // }
-                //                 // offset+=12;
-                //             }
-                //             //pc_pub.publish(pc_msg);
-                //         }
-                //     }     
-                // return;
-                // }
+                            for (int offset=0; offset<4; offset++){
+                                pc_msg.data[p0+offset] = castPtr_x[offset];
+                                pc_msg.data[p0+4+offset] = castPtr_y[offset];
+                                pc_msg.data[p0+8+offset] = castPtr_z[offset];
+                            }
+                            
+                            p0+=12;
+                        }
+                        pc_pub.publish(pc_msg);
+                    }
+                       
+                return;
+                }
 
 
             private:
